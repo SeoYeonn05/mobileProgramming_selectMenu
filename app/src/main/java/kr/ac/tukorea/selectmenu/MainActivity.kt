1bbdfaab253910c2e6d58d2fba65df1a6f940a0c
SeoYeon Jung <seoyeonn051@gmail.com>
2022-04-19 23:30:45 +0900

    fix:class remove

--- app/src/main/java/kr/ac/tukorea/selectmenu/MainActivity.kt
+++ app/src/main/java/kr/ac/tukorea/selectmenu/MainActivity.kt
@@ -1,19 +1,31 @@
 package kr.ac.tukorea.selectmenu
 
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
-    lateinit var myHelper: myDBHelper
-    lateinit var menuImageView: ImageView
+    lateinit var myHelper:myDBHelper
+    lateinit var menuImageView:ImageView
     lateinit var menuSelectBtn:Button
+    lateinit var menuAddBtn:Button
+    lateinit var edtMenuResult:EditText
     lateinit var sqlDB:SQLiteDatabase
+
+    var curNum:Int=1
+    //음식 이름과 사진 url Array
+    var foodArray=arrayOf(arrayOf("비빔밥", "R.drawable.bibimbap"),arrayOf("라면", "R.drawable.ramen"), arrayOf("파스타", "R.drawable.pasta"), arrayOf("햄버거", "R.drawable.hamburger")))
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,17 +33,51 @@ class MainActivity : AppCompatActivity() {
 
         menuImageView.findViewById<ImageView>(R.id.randomFoodImage)
         menuSelectBtn.findViewById<Button>(R.id.selectBtn)
-        myHelper=myDBHelper(this)
+        edtMenuResult.findViewById<EditText>(R.id.edtMenuResult)
+        menuAddBtn.findViewById<Button>(R.id.addBtn)
 
-    }
-    inner class myDBHelper(context: Context):SQLiteOpenHelper(context, "menuDB", null, 1){
-        override fun onCreate(p0: SQLiteDatabase?) {
-            p0!!.execSQL("CREATE TABLE menuDB (menuName CHAR(20), menuURL CHAR(30), menu);")
+        myHelper=myDBHelper(this)   //  menuDB 데이터베이스 생성
+
+//        var timerFunction=null;
+
+
+        firstMenuSave()
+//        timerFunction=setInterval(changeImage(), 1000)
+
+        menuSelectBtn.setOnClickListener {
+
         }
 
-        override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-            p0!!.execSQL("DROP TABLE IF EXISTS menuDB")
-            onCreate(p0)
+    }
+    private fun firstMenuSave(){    //  처음 음식 메뉴 DB에 저장
+        sqlDB=myHelper.writableDatabase
+        for(i in 1..curNum step 1){
+            sqlDB.execSQL("INSERT INTO menuDB VALUES ( '"+foodArray!![i][0]+"',"+foodArray!![i][1]+");")
+        }
+        sqlDB.close()
+    }
+
+    fun changeImage(){
+        var url=getResources().getIdentifier(readDatabase(), null, null)
+        menuImageView.setImageResource(url)
+    }
+
+    fun readDatabase():String{
+        var range=(1..curNum).random()
+
+        sqlDB=myHelper.readableDatabase
+        var cursor:Cursor   //menuDB 테이블에서 가져온 데이터를 커서에 전달
+        cursor=sqlDB.rawQuery("SELECT * FROM menuDB WHERE menuNo=${range};", null)
+        return cursor.toString()
+    }
+
+    inner class myDBHelper(context: Context):SQLiteOpenHelper(context, "menuDB", null, 1){
+        override fun onCreate(db: SQLiteDatabase?) {
+            db!!.execSQL("CREATE TABLE menuDB (menuNo INTERGER(10), menuName CHAR(20), menuURL CHAR(30));")
+        }
+        override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+            db!!.execSQL("ALTER TABLE menuDB EXISTS menuDB")
+            onCreate(db)
         }
     }
 }
